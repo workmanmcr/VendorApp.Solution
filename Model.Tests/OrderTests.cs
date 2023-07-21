2eaa292b5c5b37c942e8f3fcc68c3100fa289806
workmanmcr <workmanmcr@gmail.com>
2023-07-21 12:15:13 -0700

    test updating description pass

--- Model.Tests/OrderTests.cs
+++ Model.Tests/OrderTests.cs
@@ -20,6 +20,33 @@ namespace VendorApp.Tests
       Order newOrder = new Order ("test","test",5,1,"bob",2);
       Assert.AreEqual (typeof (Order), newOrder.GetType ());
     }
+     [TestMethod]
+    public void GetTitle_ReturnsTitle_String() 
+    {
+      //Arrange
+      string title = "bob's smokehouse";
+
+      //Act
+      Order newVendor = new Order (title,"order Croissants",30,1,"bob",2);
+      string result = newVendor.Title;
+
+      //Assert
+      Assert.AreEqual (title, result);
+    }
+     [TestMethod]
+    public void SetDescription_SetDescription_String () {
+      //Arrange
+      string description = "croissants.";
+      Order newOrder = new Order ("bobs Store",description,30,2,"bob",1);
+
+      //Act
+      string updatedDescription = "sourdough bread";
+      newOrder.Description = updatedDescription;
+      string result = newOrder.Description;
+
+      //Assert
+      Assert.AreEqual (updatedDescription, result);
+  }
   }
 
 }
